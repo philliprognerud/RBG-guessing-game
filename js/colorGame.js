7dbbdf39b521bee3f26fd008845456a46666e133
Phillip Rognerud <philliprognerud2@yahoo.com>
2017-06-13 23:45:31 -0700

    Added more features
    Colored squares, clickable features

--- js/colorGame.js
+++ js/colorGame.js
@@ -1,1 +1,27 @@
-alert('connected');
+var colors = [
+  "rgb(255, 0, 0)",
+  "rgb(255, 255, 0)",
+  "rgb(0, 255, 0)",
+  "rgb(0, 255, 255)",
+  "rgb(0, 0, 255)",
+  "rgb(255, 0, 255)",
+]
+
+var squares = document.querySelectorAll(".square");
+var pickedColor = colors[3];
+var colorDisplay = document.querySelector("#colorDisplay");
+
+colorDisplay.textContent = pickedColor;
+
+squares.forEach(function(square, i){
+  square.style.backgroundColor = colors[i];
+  square.addEventListener("click", function(){
+    var clickedColor = this.style.backgroundColor;
+
+    if(clickedColor === pickedColor){
+      alert("correct");
+    } else {
+      alert("wrong");
+    }
+  });
+});
